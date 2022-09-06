d66b960f0bc0b95bf441ab7871372e94ee20e6fb
Harry <harrypham_@hotmail.com>
2022-09-07 00:51:03 +1000

    edit css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-
+import { FaPhoneAlt } from 'react-icons/fa'
 
 function App() {
-
   const [contacts, setContacts] = useState([])
   const [search, setSearch] = useState('')
   const [searchResults, setSearchResults] = useState([])
@@ -13,15 +12,15 @@ function App() {
     axios.get('https://jsonplaceholder.typicode.com/users')
     .then(res => {
       setContacts(res.data)
-      console.log(contacts)
     })
   }, [])
 
+  useEffect(() => {
+    setSearchResults(contacts.filter(contact => contact.name.toLowerCase().includes(search.toLowerCase()) || contact.phone.replace(/[() -]/g, '').includes(search) || contact.email.toLowerCase().includes(search.toLowerCase())))
+  }, [search])
+
   const handleSearch = (e) => {
     setSearch(e.target.value)
-    console.log(search)
-    setSearchResults(contacts.filter(contact => contact.name.includes(search) || contact.phone.includes(search)))
-    console.log(searchResults)
   }
 
   return (
@@ -31,17 +30,25 @@ function App() {
       </header>
       <ul className='contact-card-list'>
         {search.length === 0 ? contacts.map((contact, i) => {
-          return <li key={i} className='contact-card'>
-            <h4 className='contact-card-name'>{contact.name}</h4>
-            <div>{contact.phone}</div>
-          </li>
+          return (
+            <li key={i} className='contact-card'>
+              <h4 className='contact-card-name'>{contact.name}</h4>
+              <div className='contact-card-phone'><span className='contact-list-phone-icon'><FaPhoneAlt/></span>{contact.phone}</div>
+              <section className='contact-more-details'>
+                <button className='contact-more-details-button'>See Details</button>
+              </section>
+            </li>)
         })
       :
       searchResults.map((contact, i) => {
-        return <li key={i} className='contact-card'>
-        <h4 className='contact-card-name'>{contact.name}</h4>
-        <div>{contact.phone}</div>
-      </li>
+        return (
+          <li key={i} className='contact-card'>
+            <h4 className='contact-card-name'>{contact.name}</h4>
+            <div className='contact-card-phone'><span className='contact-list-phone-icon'><FaPhoneAlt/></span>{contact.phone}</div>
+            <section className='contact-more-details'>
+              <button className='contact-more-details-button'>See Details</button>
+            </section>
+          </li>)
       })
       }
       </ul>
